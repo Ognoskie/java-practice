b89a4d4aed58b392a4edc7c26bf557091caa090c
Braden Wittkop <bradenjwittkop@gmail.com>
2023-07-19 18:44:13 -0500

    added java syntax

--- JavaPractice.java
+++ JavaPractice.java
@@ -1,5 +1,11 @@
 public class JavaPractice {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello java");
+        int myFirstNumber = 5;
+        System.out.println(myFirstNumber);
+        System.out.println("myFirstNumber");
+        myFirstNumber = 10;
+        System.out.println(myFirstNumber);
     }
 }
