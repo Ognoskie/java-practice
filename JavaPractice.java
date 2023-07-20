c0f694a2dd18a2b11f9e617df1f2c408e778024c
Braden Wittkop <bradenjwittkop@gmail.com>
2023-07-19 19:13:20 -0500

    added more java exercises

--- JavaPractice.java
+++ JavaPractice.java
@@ -9,5 +9,21 @@ public class JavaPractice {
         System.out.println(myFirstNumber);
         myFirstNumber = 1000;
         System.out.println(myFirstNumber);
+        myFirstNumber = 10 + 5;
+        System.out.println(myFirstNumber);
+        myFirstNumber = (10 + 5) + (2 * 10);
+        System.out.println(myFirstNumber);
+        int mySecondNumber = 12;
+        int myThirdNumber = 6;
+        System.out.println(mySecondNumber);
+        System.out.println(myThirdNumber);
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        System.out.println(myTotal);
+        myThirdNumber = myFirstNumber * 2;
+        System.out.println(myThirdNumber);
+        myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        System.out.println(myTotal);
+        int myLastOne = 1000 - myTotal;
+        System.out.println(myLastOne);
     }
 }
